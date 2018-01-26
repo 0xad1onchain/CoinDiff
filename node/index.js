83b8c27ae432aba20b514431a8eb98d8c1f40a1b
Aditya Gupta <adi.gupta13@gmail.com>
2018-01-26 15:30:40 +0530

    Adding web interface

---
+++ node/index.js
@@ -0,0 +1,23 @@
+var express = require("express"),
+    request = require("request"),
+    bodyParser  =   require("body-parser"),
+    app         =   express();
+
+app.set("view engine","ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended : true}));
+
+var priceList
+var url = 'http://127.0.0.1:5000/price'
+request.get(url,function(err,res,body){
+    priceList = JSON.parse(body);
+    console.log(priceList['stats']['BTC'])
+});
+
+app.get("/",function(req,res){
+    res.render("index",{priceList:priceList});
+});
+
+app.listen(8080,function(){
+    console.log("Server is running")
+})
